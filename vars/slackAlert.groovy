ecd1318890a01168b0b09de4305a962e2e846a1f
Syed Muhammad Danial <102377018+syed-danial@users.noreply.github.com>
2023-10-06 04:54:13 +0500

    Create slackAlert.groovy

---
+++ vars/slackAlert.groovy
@@ -0,0 +1,18 @@
+def call(String successMessage='', String failureMessage='', String otherMessage='') {
+  try{
+    def buildStatus = currentBulid.currentResult
+    def message = buildStatus == 'SUCCESS' ? successMessage :
+        buildStatus == 'FAILURE' : ? failureMessage : otherMessage
+    if(message) {
+      slackSend(
+        color : buildStatus == 'SUCCESS' ? '#36a64f' :
+                buildStatus == 'FAILURE' ? 'danger' : '#ffa550' , 
+        message : message, channel = "jenkins-status"
+      )
+    }
+  }  
+  catch (exception e){
+    currentBuild.result = 'FAILURE'
+    error("Failed to send Slack notification: ${e.message}")
+  }
+}
