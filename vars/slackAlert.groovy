03ab10050396ea557c549f274a2bbb374b8ea952
Syed Muhammad Danial <102377018+syed-danial@users.noreply.github.com>
2023-10-06 05:20:59 +0500

    Update slackAlert.groovy

--- vars/slackAlert.groovy
+++ vars/slackAlert.groovy
@@ -1,18 +1,22 @@
-def call(String successMessage='', String failureMessage='', String otherMessage='') {
-  try{
-    def buildStatus = currentBulid.currentResult
-    def message = buildStatus == 'SUCCESS' ? successMessage :
-        buildStatus == 'FAILURE' ? failureMessage : otherMessage
-    if(message) {
-      slackSend(
-        color : buildStatus == 'SUCCESS' ? '#36a64f' :
-                buildStatus == 'FAILURE' ? 'danger' : '#ffa550' , 
-        message : message, channel = "jenkins-status"
-      )
-    }
-  }  
-  catch (Exception e){
-    currentBuild.result = 'FAILURE'
-    error("Failed to send Slack notification: ${e.message}")
-  }
+// def call(String successMessage='', String failureMessage='', String otherMessage='') {
+//   try{
+//     def buildStatus = currentBulid.currentResult
+//     def message = buildStatus == 'SUCCESS' ? successMessage :
+//         buildStatus == 'FAILURE' ? failureMessage : otherMessage
+//     if(message) {
+//       slackSend(
+//         color : buildStatus == 'SUCCESS' ? '#36a64f' :
+//                 buildStatus == 'FAILURE' ? 'danger' : '#ffa550' , 
+//         message : message, channel = "jenkins-status"
+//       )
+//     }
+//   }  
+//   catch (Exception e){
+//     currentBuild.result = 'FAILURE'
+//     error("Failed to send Slack notification: ${e.message}")
+//   }
+// }
+
+def call(Map params) {
+    new sharedLibrary.SlackAlert(params).send()
 }
